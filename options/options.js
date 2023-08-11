804314b3ee1cc897951e5abeb4ea714f01a5ecf6
Adam <aschnare2@gmail.com>
2023-08-11 16:41:42 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "tommy-test-42778",
-  url: "https://tommy-test-42778.botics.co",
-  api: "https://tommy-test-42778.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "tommy-test-42778",
+  "url": "https://tommy-test-42778.botics.co",
+  "api": "https://tommy-test-42778.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "login2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
